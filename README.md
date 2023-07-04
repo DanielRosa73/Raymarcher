d036f79a4877821b1462a6ebe34f8db5ecab5ed7
daniel.rosa <daniel.rosa@epita.fr>
2023-07-04 14:49:00 +0200

    screenshots

--- README.md
+++ README.md
@@ -6,14 +6,13 @@
 ```./raymarcher```
 ## Screenshots
 
-![Example Screenshot](images/example.png)
+![Example Screenshot](screenshots/inside_the_mandelbulb.png)
+![Example Screenshot](screenshots/space.png)
+![Example Screenshot](screenshots/maxibestoffplus.png)
+![Example Screenshot](screenshots/bongbing.png)
+![Example Screenshot](screenshots/minecraft.png)
+![Example Screenshot](screenshots/mandlebulb_afar.png)
+![Example Screenshot](screenshots/peanut.png)
 
-![Example Screenshot](images/example.png)
 
-![Example Screenshot](images/example.png)
 
-![Example Screenshot](images/example.png)
-
-![Example Screenshot](images/example.png)
-
-![Example Screenshot](images/example.png)
--- src/main.cpp
+++ src/main.cpp
@@ -44,7 +44,6 @@ void write_ppm(const std::string& filename, const std::vector<std::vector<Color>
     file.close();
 }
 
-
 /*
 int main() {
     int width = 800;
@@ -54,80 +53,36 @@ int main() {
     Light light(Vector3(1.0f, 3.0f, 1.0f), Color(1.0f, 1.0f, 1.0f), 0.0f);
 
     Material mat1(1, 1000,0.2f);
-    Material mat11(1, 1000,0.2f);
-    Material mat2(1, 10000,0.0f);
-    Material mat3(1, 10000,0.0f);
-    Material mat4(1, 10000,0.0f);
-    Material mat5(1, 10000,0.0f);
-    Material mat6(1, 10000,0.0f);
-    Material mat7(1, 10000,0.0f);
-    Material mat8(1, 10000,0.0f);
-    Material mat(1, 10000,0.0f);
-
-    
+    Material soft(1,100000,0);
 
 
-    Sphere sphere1(Vector3(-1.2, 0.25f, -1.6f), 1.6f, Color(0.1f, 0.1f, 0.9f), mat1);
+
+    Sphere sphere1(Vector3(-1.2, 0.25f, -1.6f), 1.6f, Color(0.37f, 0.06f, 0.57f), mat1);
     Sphere sphere11(Vector3(1.2f, 0.25f, -1.6f), 1.6f, Color(0.12f, 0.28f, 0.59f), mat1);
-    Sphere sphere2(Vector3(0.0f, 0.4f, 1.0f), 0.5f, Color(0.0f, 1.0f, 1.0f), mat2);
-    Sphere sphere3(Vector3(-2.3f, -1.0f, 1.9f), 0.5f, Color(1.0f, 0.0f, 1.0f), mat3);
-    Sphere sphere4(Vector3(-0.5f, -1.0f, 3.2f), 0.5f, Color(0.2f, 0.8f, 0.2f), mat4);
-    Sphere sphere5(Vector3(0.5f, -1.0f, 0.6f), 0.5f, Color(1.0f, 0.64f, 0.0f), mat5);
-    Sphere sphere6(Vector3(0.75f, -1.0f, 2.3f), 0.5f, Color(0.3f, 0.4f, 0.3f), mat6);
-    Sphere sphere7(Vector3(2.4f, -1.0f, 1.0f), 0.5f, Color(0.1f, 0.5f, 0.4f), mat7);
-    Sphere sphere8(Vector3(2.0f, -1.0f, 2.7f), 0.5f, Color(1.0f, 0.84f, 0.4f), mat8);
 
-    Peanut peanut(sphere1, sphere11, 0.4f);
+    Peanut peanut(sphere1, sphere11, 0.8f);
+
+
     
     Scene scene;
     scene.setCamera(camera);
+    //scene.addObject(std::make_shared<Peanut>(peanut));
     scene.addObject(std::make_shared<Peanut>(peanut));
-    scene.addObject(std::make_shared<Sphere>(sphere2));
+    
+
+
+
+
 
     
     
     scene.addLight(std::make_shared<Light>(light));
     
-    scene.addObject(std::make_shared<Sphere>(sphere1));
-    scene.addObject(std::make_shared<Sphere>(sphere11));
-    scene.addObject(std::make_shared<Sphere>(sphere2));
-    scene.addObject(std::make_shared<Sphere>(sphere3));
-    scene.addObject(std::make_shared<Sphere>(sphere4));
-    scene.addObject(std::make_shared<Sphere>(sphere5));
-    scene.addObject(std::make_shared<Sphere>(sphere6));
-    scene.addObject(std::make_shared<Sphere>(sphere7));
-    scene.addObject(std::make_shared<Sphere>(sphere8));
-    
-
-    
-    
-    Vector3 cubeCenterL(-5, 1 , -3);
-    Vector3 cubeCenterR(5, 1 , -3);
-    Vector3 cubeCenterBack(0, 1 , -5);
-    Vector3 cubeCenterBot(0, -2 , 0);
-    Vector3 cubeCenterTop(0, 5 , 0);
-    Vector3 cubeDimensions(1, 10, 10);
-    Vector3 cubeDimensions2(10, 10, 1);
-    Vector3 cubeDimensions3(10, 1, 10);
-    Color cubeColor1(0.05, 0.05, 0.05);
-    Color cubeColor2(0.05, 0.05, 0.05);
-    
-    std::shared_ptr<Cube> cubeL = std::make_shared<Cube>(cubeCenterL, cubeDimensions, cubeColor1, mat);
-    std::shared_ptr<Cube> cubeR = std::make_shared<Cube>(cubeCenterR, cubeDimensions, cubeColor1, mat);
-    std::shared_ptr<Cube> cubeBack = std::make_shared<Cube>(cubeCenterBack, cubeDimensions2, cubeColor2, mat);
-    std::shared_ptr<Cube> cubeBot = std::make_shared<Cube>(cubeCenterBot, cubeDimensions3, cubeColor2, mat);
-    std::shared_ptr<Cube> cubeTop = std::make_shared<Cube>(cubeCenterTop, cubeDimensions3, cubeColor2, mat);
-    scene.addObject(cubeL);
-    scene.addObject(cubeR);
-    scene.addObject(cubeBack);
-    scene.addObject(cubeBot);
-    scene.addObject(cubeTop);
-    
 
 
     std::vector<std::vector<Color>> framebuffer(width, std::vector<Color>(height));
     Raymarcher raymarcher;
-    raymarcher.render_MSAA(scene, framebuffer);
+    raymarcher.render_antialiasing(scene, framebuffer);
 
     write_ppm("output.ppm", framebuffer);
 
@@ -138,7 +93,6 @@ int main() {
 
 
 // Main for the mandelbulb
-/*
 int main() {
     int width = 800;
     int height = 600;
@@ -147,15 +101,15 @@ int main() {
     //Camera camera(Vector3(0.0f, 0.0f, 0.4f), Vector3(0.0f, 0.0f, -1.0f), Vector3(0.0f, 1.0f, 0.0f), 60.0f, float(width) / float(height));
     
     //Zoomed in
-    //Camera camera(Vector3(0.0f, 1.0f, 0.2f), Vector3(0.0f, 0.0f, -1.0f), Vector3(0.0f, 1.0f, 0.0f), 60.0f, float(width) / float(height));
+    Camera camera(Vector3(0.0f, 1.0f, 0.2f), Vector3(0.0f, 0.0f, -1.0f), Vector3(0.0f, 1.0f, 0.0f), 60.0f, float(width) / float(height));
 
     //Bottom view
     //Camera camera(Vector3(0.0f, 0.0f, 2.4f), Vector3(0.0f, 0.0f, -1.0f), Vector3(0.0f, 1.0f, 0.0f), 60.0f, float(width) / float(height));
 
     //Full View
-    Camera camera(Vector3(0.0f, 2.0f, 2.4f), Vector3(0.0f, 0.0f, -1.0f), Vector3(0.0f, 1.0f, 0.0f), 60.0f, float(width) / float(height));
+    //Camera camera(Vector3(0.0f, 2.0f, 2.4f), Vector3(0.0f, 0.0f, -1.0f), Vector3(0.0f, 1.0f, 0.0f), 60.0f, float(width) / float(height));
 
-    Light light(Vector3(0.0f, 7.0f, 1.5f), Color(1.0f, 1.0f, 1.0f), 0.2f);
+    Light light(Vector3(0.0f, 7.0f, 1.5f), Color(1,1,1), 0.2f);
 
     Material mat(1,10000,0.0f);
 
@@ -164,7 +118,7 @@ int main() {
     scene.addLight(std::make_shared<Light>(light));
     
 
-    Mandelbulb mandelbulb(Vector3(0,0,0),1.0f, 8, Color(1.0f,0.0f,1.0f),mat);
+    Mandelbulb mandelbulb(Vector3(0,0,0),1.0f, 8, Color(0.43f, 0.34f, 0.24f),mat);
     scene.addObject(std::make_shared<Mandelbulb>(mandelbulb));
 
     std::vector<std::vector<Color>> framebuffer(width, std::vector<Color>(height));
@@ -175,7 +129,6 @@ int main() {
 
     return 0;
 }
-*/
 
 
 //Main for Space
@@ -215,10 +168,11 @@ int main() {
     write_ppm("output.ppm", framebuffer);
     return 0;
 }
+
 */
 
-
 //Main for cube
+/*
 int main() {
     int width = 800;
     int height = 600;
@@ -266,7 +220,8 @@ int main() {
 
     std::vector<std::vector<Color>> framebuffer(width, std::vector<Color>(height));
     Raymarcher raymarcher;
-    raymarcher.render(scene, framebuffer);
+    raymarcher.render_MSAA(scene, framebuffer);
     write_ppm("output.ppm", framebuffer);
     return 0;
 }
+*/
